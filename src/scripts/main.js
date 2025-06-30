0d64d801a45ac04ae9456dea305e86ae61c32a62
Vladyslav Stepanenko <vlad.step.186@gmail.com>
2025-06-30 10:49:19 +0300

    added label

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((item, index) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  let id = item.id;
+
+  if (item.id) {
+    label.setAttribute('for', id);
+  } else {
+    id = `input-${item.name || 'field'}-${index}`;
+    item.id = id;
+    label.setAttribute('for', id);
+  }
+
+  label.textContent = item.name || `Field ${index + 1}`;
+  item.parentNode.insertBefore(label, item);
+
+  const names = item.name;
+  const capitalized = names.charAt(0).toUpperCase() + names.slice(1);
+
+  if (!item.hasAttribute('placeholder')) {
+    item.setAttribute('placeholder', capitalized);
+  }
+});
